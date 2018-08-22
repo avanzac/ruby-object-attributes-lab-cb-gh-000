cf1dcfd4c74f8d75b5d3a6de85ba72ce5d18db28
darren mcgregor <darren.mcgregor@avanzac.co.uk>
2018-08-22 14:23:37 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Persondef initialize(name)
+  @name = name
+end
+def name
+  @name
+end
+def name=(new_name)
+  @name = new_name
+end
+
+def initialize(job)
+  @job = job
+end
+def job
+  @job
+end
+def job=(new_job)
+  @job = new_job
+end
